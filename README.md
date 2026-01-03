9edf899ff427e814e5c5675fd93c780dd44319b6
Pergl <per.glansa@gmail.com>
2026-01-03 10:58:48 +0100

    Första commiten av kod

---
+++ README.md
@@ -0,0 +1,26 @@
+# inloggningsfunktion – Testautomatisering
+
+Detta projekt är en del av kursen **Testautomatisering** och innehåller automatiserade tester
+för en inloggningsfunktion på en färdig webbsida, skrivna i **Python** med **Selenium WebDriver**
+och **pytest**.
+
+## Testobjekt
+Webbsidan som testas:
+https://www.saucedemo.com/
+
+## Tekniker och verktyg
+- Python 3.13
+- Selenium WebDriver
+- pytest
+- PyCharm
+- GitHub
+
+## Projektstruktur
+inloggningsfunktion/
+│└──.venv
+├── Uppgift1/
+│ └── positivetest.py
+│
+|
+├── requirements.txt
+└── README.md
---
+++ Uppgift1/test_login.py
@@ -0,0 +1,40 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+from selenium.webdriver.common.by import By
+
+class TestLogin():
+
+    def test_positive_login(self, driver):
+        # Go to webpage
+        driver.get("https://www.saucedemo.com")
+        time.sleep(2)
+
+        # Type username student into Username field
+        username_locator = driver.find_element(By.ID, "user-name")
+        username_locator.send_keys("standard_user")
+
+        # Type password secret_sauce into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("secret_sauce")
+
+        driver.find_element(By.ID, "login-button").click()
+
+        WebDriverWait(driver, 10).until(
+            EC.url_contains("inventory.html")
+        )
+
+        # Verify new page has url https://www.saucedemo.com/inventory.html
+        assert "inventory.html" in driver.current_url
+        inventory = driver.find_element(By.CLASS_NAME, "inventory_list")
+        assert inventory.is_displayed()
+

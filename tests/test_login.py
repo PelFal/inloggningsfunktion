62c68dab8baedd1e1c0820f07bb4fda4fa687a16
Pergl <per.glansa@gmail.com>
2026-01-08 09:39:20 +0100

    lagt till options för webdriver

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,7 +13,7 @@ def driver():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 

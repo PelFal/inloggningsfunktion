61e87e5c2a44b8732416849bfc5f552e5d851919
Pergl <per.glansa@gmail.com>
2026-01-28 09:53:02 +0100

    test igen

--- tests/test_login.py
+++ tests/test_login.py
@@ -70,3 +70,5 @@ class TestLogin():
         assert error_message_locator.is_displayed()
         assert "Epic sadface" in error_message_locator.text
         print("Felmeddelande: ", error_message_locator.text)
+
+# testar igen

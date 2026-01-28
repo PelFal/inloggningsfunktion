90e94aca2866e3157c7b1384a629f8772b17564e
Pergl <per.glansa@gmail.com>
2026-01-28 16:00:52 +0100

    fail i github med 403:1

--- tests/test_api_integration.py
+++ tests/test_api_integration.py
@@ -9,3 +9,5 @@ BASE_URL = "https://fakestoreapi.com/products"
 def test_api_can_be_called_200():
     response = requests.get(BASE_URL)
     assert response.status_code == 200
+
+#kommentar för att pusha

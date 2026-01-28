ec91fb71d1486b16995a558efa8fd669dc9ae40e
Pergl <per.glansa@gmail.com>
2026-01-28 15:56:11 +0100

    fail i github med 403

--- tests/test_api_integration.py
+++ tests/test_api_integration.py
@@ -4,9 +4,8 @@ import pytest
 pytestmark = pytest.mark.api
 
 
-BASE_URL = "https://fakestoreapi.com"
+BASE_URL = "https://fakestoreapi.com/products"
 
-def test_api_can_be_called():
+def test_api_can_be_called_200():
     response = requests.get(BASE_URL)
     assert response.status_code == 200
-#testmotgitubhactions
